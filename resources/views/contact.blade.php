1aa0bf76a6dbc6dfb1ce3ff54a2a61671253571b
Kim Fui Leung <93153597+isaackk60@users.noreply.github.com>
2024-03-07 18:05:20 +0000

    adjust css of contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,20 +9,20 @@
     </div>
 </div>
 
-<div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto pt-15 border-t border-gray-200">
+<div class="sm:grid grid-cols-2 gap-10 w-5/6 mx-auto pt-15 border-t border-gray-200">
                 
     <div class="flex flex-col justify-center">
         
     <p class="text-lg text-gray-700 leading-relaxed mb-6">Welcome to TechNewsWorld's Contact Us page. We're delighted to hear from you! Whether you have a question, suggestion, or just want to say hello, feel free to reach out to us using the form below. Our team is committed to providing you with the best possible experience and will respond to your inquiry as soon as possible.</p>
 
-<ul class="list-disc pl-6">
-    <li class="text-gray-700">Address: 123 Tech Street, Techville, TX 12345</li>
-    <li class="text-gray-700">Phone: +1 (123) 456-7890</li>
-    <li class="text-gray-700">Email: info@technewsworld.com</li>
+<ul class="list-disc pl-6 mb-6">
+    <li class="text-gray-700 leading-normal text-base font-normal">Address: Dublin Rd, Marshes Upper, Dundalk, Co. Louth, A91 K584</li>
+    <li class="text-gray-700 leading-normal text-base font-normal">Phone: +353 083 090 4444</li>
+    <li class="text-gray-700 leading-normal text-base font-normal">Email: info@technewsworld.com</li>
 </ul>
     </div>
     <div class="image-padding">
-        <img src="https://ats.org/wp-content/uploads/2020/04/Index-High-Tech-Future.jpg" alt="">
+        <img src="https://media.istockphoto.com/id/1346125184/photo/group-of-successful-multiethnic-business-team.jpg?s=612x612&w=0&k=20&c=5FHgRQZSZed536rHji6w8o5Hco9JVMRe8bpgTa69hE8=" alt="">
     </div>
 </div>
 
